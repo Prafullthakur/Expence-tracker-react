7a095bfd8ee7b0a2e7300ab134ef90f8cf1ecb54
Prafull thakur <prafullchauhan49@gmail.com>
2020-07-01 16:23:42 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpences } from "./components/IncomeExpences";
+import { Transactionlist } from "./components/Transactionlist";
+import { AddTransaction } from "./components/AddTransaction";
 import "./App.css";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpences />
+        <Transactionlist />
+        <AddTransaction />
       </div>
     </div>
   );
